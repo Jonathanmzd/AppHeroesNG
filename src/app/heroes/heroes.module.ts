8b5b652c3a9723a80fbd76fb7fd15574b8b0dfda
Jonathan Ardila <mized2009@gmail.com>
2021-07-13 19:58:52 -0500

    181. Material Sidenav, Toolbar e iconos

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+
 
 
 
@@ -24,6 +26,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
