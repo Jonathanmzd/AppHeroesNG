60f9e2ea9ef6b14c6a3ed8d0c7121289dba23b21
Jonathan Ardila <mized2009@gmail.com>
2021-07-15 15:29:31 -0500

    190. Tarea - Ruta Héroe y Editar Héroe = crear rutas en routerLink y activateRoute

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { identity } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(({id}) => console.log (id));
   }
 
 }
